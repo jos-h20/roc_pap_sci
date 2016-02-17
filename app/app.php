31e4747fb3f4b023896777cc3ad7fa70b9170d2d
Josh Overly and Ryan Brown <joshoverly@yahoo.com>
2016-02-17 13:37:48 -0800

    Add css to jumbotron.

--- app/app.php
+++ app/app.php
@@ -2,6 +2,12 @@
 	require_once __DIR__.'/../vendor/autoload.php';
 	require_once __DIR__.'/../src/RPS.php';
 
+	// session_start();
+	//
+    // if (empty($_SESSION['list_of_inputs'])){
+    //     $_SESSION['list_of_inputs'] = array();
+    // }
+
 	$app = new Silex\Application();
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
